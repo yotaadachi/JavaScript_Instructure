128d62b06ae874447b63007d0108c9ca8efffc77
yotaadachi <yota961219@gmail.com>
2022-02-20 19:25:41 +0900

    040_ホイスティング学習

--- 020_実行環境/040_ホイスティング/start/main.js
+++ 020_実行環境/040_ホイスティング/start/main.js
@@ -1,4 +1,12 @@
-a();
-function a() {
+const a = function a() {
+  console.log(c);
+  var c = 1;
+  d();
+  function d() {
+    console.log('d is called');
+  }
   console.log('a is called');
 }
+a();
+console.log(b);
+var b = 0;
