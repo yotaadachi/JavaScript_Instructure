6bdcaf5c631774c5fdd8ec50d064803c18346c78
yotaadachi <yota961219@gmail.com>
2022-03-13 14:52:19 +0900

    920_修了確認_bindとthis学習

--- 050_関数とオブジェクト１/920_修了確認_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/920_修了確認_bindとthis/start/main.js
@@ -1,6 +1,6 @@
 const person = {
     name: 'Tom',
-    bye: () => {
+    bye() {
         console.log('Bye ' + this.name);
     },
     hello: function (greeting) {
@@ -22,10 +22,21 @@ const person = {
      * ２．アロー関数
      * ３．thisを一旦変数に代入
      */
+    hello1s() {
+        // setTimeout(this.hello.bind(this, 'hello'), 1000);
 
+        // setTimeout(() => {
+        //     this.hello('hello');
+        // }, 1000);
 
-    
+        const _this = this;
+        setTimeout(function() {
+            _this.hello('hello');
+        })
+    }
+
 }
+person.hello1s();
 
 /**
  * 問題１：
@@ -33,7 +44,7 @@ const person = {
  * と出力されるように、以下のコード
  * の記載を変更しましょう。
  */
-setTimeout(person.hello, 1000);
+setTimeout(person.hello.bind(person, "hello"), 1000);
 
 /**
  * 問題２：
@@ -41,7 +52,7 @@ setTimeout(person.hello, 1000);
  * と出力されるように、
  * 以下のコードを変更してください。
  */
-alert(person.hello);
+alert(person.hello('hello'));
 
 /**
  * 問題３：
