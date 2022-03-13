afd1e4d0ae565aa7fa7eff45a0a02fc961f7757b
yotaadachi <yota961219@gmail.com>
2022-03-13 14:29:02 +0900

    910_修了確認_コールバック関数学習

--- 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
+++ 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
@@ -12,19 +12,25 @@ const person = {
     }
 }
 
-// setTimeout(/** ここに追記 */, 1000);
+setTimeout(function() {
+    const hello = person.hello();
+    console.log(hello);
+}, 1000);
 
 /**
  * 問題２：
  * setTimeoutの実行から１秒後にブラウザの
  * ダイアログに'hello Tom'と表示されるように
  * 実装してみましょう。
- * 
+ *
  * ※必ずperson.helloメソッドは解答内で使用してください。
  * ※alertは第一引数に渡した文字列を画面のダイアログに表
  * 示する関数です。
  */
-
+// setTimeout(function() {
+//     const hello = person.hello();
+//     alert(hello);
+// }, 1000);
 
 /**
  * 問題３：
@@ -46,7 +52,7 @@ function after1s(callack) {
 }
 
 // この時点で実行します。
-// after1s(obj.greeting);
+after1s(obj.greeting);
 
 // この後でgreetingを書き換えます。
 obj.greeting = function() {
@@ -63,32 +69,32 @@ obj.greeting = function() {
  * 
  * ※コールバック関数を用いて実装してください。
  */
-function calcFactory(val) {
+function calcFactory(val, callback) {
     return {
         plus: function(target) {
             const newVal = val + target;
-            console.log(`${val} + ${target} = ${newVal}`);
+            callback(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
         minus: function(target) {
             const newVal = val - target;
-            console.log(`${val} - ${target} = ${newVal}`);
+            callback(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
         multiply: function(target) {
             const newVal = val * target;
-            console.log(`${val} x ${target} = ${newVal}`);
+            callback(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
         divide: function(target) {
             const newVal = val / target;
-            console.log(`${val} / ${target} = ${newVal}`);
+            callback(`${val} / ${target} = ${newVal}`);
             val = newVal;
         }
     };
 }
 
-const calc = calcFactory(10);
+const calc = calcFactory(10, alert);
 calc.plus(5); 
 calc.minus(3); 
 calc.multiply(3);
