a72e036949b27daa31c84b76d86f9625ade789b2
yotaadachi <yota961219@gmail.com>
2022-03-22 22:00:31 +0900

    080_反復可能オブジェクト（イテレーター）学習

--- 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
+++ 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
@@ -1,10 +1,24 @@
-const obj = {
+const items = {
 	prop1: 'value1',
 	prop2: 'value2',
 	prop3: 'value3'
 }
 
-const items = Object.entries(obj);
-for (let item of items) {
-	console.log(item);
+Object.prototype[Symbol.iterator] = function() {
+	const keys = Object.keys(this);
+	let i = 0;
+	let _this = this;
+	return {
+		next() {
+			let key = keys[i++];
+			return {
+				value:  [key, _this[key]],
+				done: i > keys.length
+			}
+		}
+	}
+}
+// const items = Object.entries(obj);
+for (let [k, v] of items) {
+	console.log([k, v]);
 }
