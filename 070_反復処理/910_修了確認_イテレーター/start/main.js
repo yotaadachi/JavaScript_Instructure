c836d5f9cb3f4f06df22fb749f50ed41929b7a0f
yotaadachi <yota961219@gmail.com>
2022-03-23 21:58:18 +0900

    910_修了確認_イテレーター学習

--- 070_反復処理/910_修了確認_イテレーター/start/main.js
+++ 070_反復処理/910_修了確認_イテレーター/start/main.js
@@ -20,13 +20,29 @@
  * 
  * の値が順番にコンソールに表示されます。
  */
+function genStep(min = 0, max = 20, step = 1) {
+  let i = min - step;
 
+  return {
+    next() {
+      i += step;
+      if (i > max) {
+        return {
+          done: true
+        }
+      } else {
+        return {
+          done: false,
+          value: i
+        }
+      }
+    }
+  }
+}
+const it = genStep(4, 10, 2);
+let a = it.next();
 
- 
-// const it = genStep(4, 10, 2);
-// let a = it.next();
-
-// while(!a.done) {
-//   console.log(a.value);
-//   a = it.next();
-// }
+while(!a.done) {
+  console.log(a.value);
+  a = it.next();
+}
