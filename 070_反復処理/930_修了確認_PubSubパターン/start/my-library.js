d04065519556b88bace58e2e644d226acc52b0b7
yotaadachi <yota961219@gmail.com>
2022-03-23 22:45:49 +0900

    930_修了確認_PubSubパターン学習

--- 070_反復処理/930_修了確認_PubSubパターン/start/my-library.js
+++ 070_反復処理/930_修了確認_PubSubパターン/start/my-library.js
@@ -39,7 +39,31 @@
  * customFn3
  * 
  */
+const events = (function() {
+	const eventStack = new Map();
 
+	return {
+		on(type, fn) {
+			const fnStack = eventStack.get(type) || new Set();
+			fnStack.add(fn);
+			eventStack.set(type, fnStack);
+		},
+		off(type, fn) {
+			const fnStack = eventStack.get(type);
+			if(fnStack && fnStack.has(fn)) {
+				fnStack.delete(fn);
+			}
+		},
+		emit(type) {
+			const fnStack = eventStack.get(type);
+			if(fnStack) {
+				for(const fn of fnStack) {
+					fn();
+				}
+			}
+		}
+	}
+})();
 class MyLibrary {
 	constructor() {
 		events.emit('beforeInit');
