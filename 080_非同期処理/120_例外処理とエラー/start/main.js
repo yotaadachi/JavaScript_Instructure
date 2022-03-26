39e4ba22a5a298783235244dfd187720f3f6cb58
yotaadachi <yota961219@gmail.com>
2022-03-26 14:08:19 +0900

    120_例外処理とエラー学習

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,11 +1,38 @@
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  if(response.ok) {
+    const json = await response.json();
+    if(!json.length) {
+      throw new NoDataError('no data found');
+    }
+    return json;
+  }
+}
 
+// カスタムエラー
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NoDataError';
+  }
+}
 
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
-// }
-
-// fetchUsers();
+async function init() {
+  try {
+    const users = await fetchUsers();
+    for(const user of users) {
+      console.log(`I'm ${user.name}, ${user.age} years old`)
+    }
+  } catch(e) {
+    if(e instanceof NoDataError) {
+      console.log(e);
+    } else {
+      console.error('Oops, something went wrong')
+    }
+    console.error(e);
+  } finally {
+    console.log('bye');
+  }
+  console.log('end');
+}
+init();
