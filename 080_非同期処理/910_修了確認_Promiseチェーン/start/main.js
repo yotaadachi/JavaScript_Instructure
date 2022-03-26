bac0eeb21d446fa1da559398a629c2d6b7e3ca80
yotaadachi <yota961219@gmail.com>
2022-03-26 14:42:51 +0900

    910_修了確認_Promiseチェーン

--- 080_非同期処理/910_修了確認_Promiseチェーン/start/main.js
+++ 080_非同期処理/910_修了確認_Promiseチェーン/start/main.js
@@ -30,8 +30,35 @@
  * 
  * ※await/asyncで記述してみてください。
  */
-async function myFetch(fileName) {
+ async function myFetch(fileName) {
 	const response = await fetch(`../json/${fileName}`);
 	const json = await response.json();
 	return json;
 }
+
+(async function() {
+	const me = await myFetch('user1.json');
+	console.log(`--${me.name}'s timeline--`);
+
+	const friendList = await myFetch(`friendsOf${me.id}.json`);
+
+	const friendIds = new Set();
+	for(const id of friendList.friendIds) {
+		friendIds.add(myFetch(`user${id}.json`));
+	}
+	const friends = await Promise.all(friendIds);
+	
+	const msgIds = new Set();
+	for(const friend of friends) {
+		msgIds.add(myFetch(`message${friend.latestMsgId}.json`));
+	}
+	const msgs = await Promise.all(msgIds);
+
+	for(const friend of friends) {
+		for(const msg of msgs) {
+			if(friend.id === msg.userId) {
+				console.log(`${friend.name} says: ${msg.message}`);
+			}
+		}
+	}
+})();
