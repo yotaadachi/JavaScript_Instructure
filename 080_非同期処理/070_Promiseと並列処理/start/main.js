0d5250fd7b7edb6f343a9e47e8a4f3e52fc6e90a
yotaadachi <yota961219@gmail.com>
2022-03-26 11:41:07 +0900

    070_Promiseと並列処理学習

--- 080_非同期処理/070_Promiseと並列処理/start/main.js
+++ 080_非同期処理/070_Promiseと並列処理/start/main.js
@@ -1,16 +1,23 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
+  return new Promise(function(reject) {
     setTimeout(function() {
       console.log(val++);
-      resolve(val);
-    }, 1000);
+      reject(val);
+    }, val * 500);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+Promise.allSettled([sleep(2), sleep(3), sleep(4)])
+.then(function(data) {
+  console.log(data);
+}).catch(function(e) {
+  console.error(e);
+});
+// sleep(0).then(function(val) {
+//   return Promise.all([sleep(2), sleep(3), sleep(4)]);
+// }).then(function(val) {
+//   console.log(val)
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
