6a68fc927cfa0ad3f03580c24b9513e47969e55b
yotaadachi <yota961219@gmail.com>
2022-03-30 16:53:48 +0900

    020_Reflect学習

--- 100_様々なオブジェクト/020_Reflect/start/main.js
+++ 100_様々なオブジェクト/020_Reflect/start/main.js
@@ -5,20 +5,31 @@ class C {
   }
 }
 
+const obj = new C(1,2)
+console.log(obj);
+const obj1 = Reflect.construct(C, [1,2]);
+console.log(obj1);
 
-// const bob = {
-//   name: 'Bob',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   }
-// }
+console.log('a' in obj)
+console.log(Reflect.has(obj, 'b'));
 
-// const tom = {
-//   name: 'Tom',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   },
-//   get hello() {
-//     return this._hello();
-//   },
-// }
+// Reflect.defineProperty
+const bob = {
+  name: 'Bob',
+  _hello: function () {
+    console.log(`hello ${this.name}`);
+  }
+}
+
+const tom = {
+  name: 'Tom',
+  _hello: function () {
+    console.log(`hello ${this.name}`);
+  },
+  get hello() {
+    console.log(this);
+    return this._hello();
+  },
+}
+tom.hello;
+Reflect.get(tom, 'hello', bob)
