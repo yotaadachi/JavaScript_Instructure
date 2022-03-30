f582c866cd5df9f02b193516ae74be247cef5add
yotaadachi <yota961219@gmail.com>
2022-03-30 16:01:42 +0900

    010_Proxy学習

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,26 @@
 const targetObj = { a: 0 };
+const handler = {
+  set: function(target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`);
+    //target[prop] = value;
+    throw new Error('cannot add prop.');
+  },
+  get: function(target, prop, receiver) {
+    if(target.hasOwnProperty(prop)) {
+    return target[prop] = value;
+    } else {
+      return '-1';
+    }
+    console.log(`[get]: ${prop}`);
+    return target[prop] = value;
+  },
+  deleteProperty: function(target, prop) {
+    console.log(`[delete]: ${prop}`);
+    delete target[prop];
+  },
+}
+const pxy = new Proxy(targetObj, handler);
+//pxy.a = 1;
+console.log(pxy.b)
+pxy.a;
+delete pxy.a;
