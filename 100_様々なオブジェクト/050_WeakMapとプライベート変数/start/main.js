131d4f71e7f4dd2dcf10edd5d81bbe3639ce1f04
yotaadachi <yota961219@gmail.com>
2022-03-31 14:33:25 +0900

    050_WeakMapとプライベート変数学習

--- 100_様々なオブジェクト/050_WeakMapとプライベート変数/start/main.js
+++ 100_様々なオブジェクト/050_WeakMapとプライベート変数/start/main.js
@@ -1,4 +1,7 @@
 import { Person } from './person.js';
 
-const tim = new Person('Tim', 23);
+const tim = new Person('Tim');
+const bob = new Person('Bob');
 tim.hello();
+bob.hello();
+// console.log(tim.name);
