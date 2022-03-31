3aea096bb4767a13b717de542f3de9c5bc63a742
yotaadachi <yota961219@gmail.com>
2022-03-31 15:38:07 +0900

    090_Array（配列）２学習

--- 100_様々なオブジェクト/090_Array（配列）２/start/main.js
+++ 100_様々なオブジェクト/090_Array（配列）２/start/main.js
@@ -1,1 +1,18 @@
 const arry = [1, 2, 3, 4, 5];
+
+arry.forEach(function(v, i, arry) {
+  console.log(v);
+});
+
+const newArry = arry.map(function(v, i, arry) {
+  console.log(v);
+  return v * 2;
+});
+
+//console.log(arry, newArry);
+
+const filterArry = arry.filter(function(v, i, arry) {
+  return i <= 1;
+});
+
+console.log(filterArry);
