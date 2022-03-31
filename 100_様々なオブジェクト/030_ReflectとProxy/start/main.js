183e6dc28e168049e470c7a8ea13c6a960783d9f
yotaadachi <yota961219@gmail.com>
2022-03-31 14:05:39 +0900

    030_ReflectとProxy学習

--- 100_様々なオブジェクト/030_ReflectとProxy/start/main.js
+++ 100_様々なオブジェクト/030_ReflectとProxy/start/main.js
@@ -1,13 +1,23 @@
-const targetObj = { a: 0 };
+const targetObj = {
+   a: 0,
+   get value() {
+     console.log(this)
+     return this.b;
+   }
+ };
 
 const handler = {
   get: function(target, prop, receiver) {
     console.log(`[get]: ${prop}`);
     if(target.hasOwnProperty(prop)) {
-      return target[prop];
+      return Reflect.get(target, prop, receiver);
     } else {
       return -1;
     }
   }
 }
 const pxy = new Proxy(targetObj, handler);
+console.log(pxy.a);
+console.log(pxy.b);
+console.log(pxy.value)
+
