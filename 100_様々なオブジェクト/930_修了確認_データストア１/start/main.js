cbe75af030ea1f1a6ed791131cc3c0eb7a64cc19
yotaadachi <yota961219@gmail.com>
2022-04-03 15:08:56 +0900

    930_修了確認_データストア１学習

--- 100_様々なオブジェクト/930_修了確認_データストア１/start/main.js
+++ 100_様々なオブジェクト/930_修了確認_データストア１/start/main.js
@@ -14,10 +14,37 @@
  * いる場合には、そのJSONからオブジェクトを復元し
  * 初期値のオブジェクトとしてください。
  */
-const KEY = 'test-data';
+ const KEY = 'test-data';
 
-// console.log('init', pxy);
-// pxy.name = 'Tom';
-// console.log('change', pxy);
-// pxy.name = 'Sun';
-// console.log('change2', pxy);
+ class DataSource {
+   static getLocal(KEY) {
+     console.log('get from local');
+     const result = localStorage.getItem(KEY);
+     return JSON.parse(result);
+   }
+ 
+   static setLocal(KEY, target) {
+     console.log('set to local');
+     const json = JSON.stringify(target);
+     localStorage.setItem(KEY, json);
+   }
+ }
+ 
+ const targetObj = DataSource.getLocal(KEY) || {};
+ 
+ const pxy = new Proxy(targetObj, {
+   set(target, prop, value, receiver) {
+     const result = Reflect.set(target, prop, value, receiver);
+ 
+     DataSource.setLocal(KEY, target);
+     
+     return result;
+   }
+ });
+ 
+ console.log('init', pxy);
+ pxy.name = 'Tom';
+ console.log('change', pxy);
+ pxy.name = 'Tim';
+ console.log('change2', pxy);
+ 
